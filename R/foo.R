d83bae55cab6f39896ec97fd6023e95999f5a701
Peng Zhao <baydap@gmail.com>
2017-05-21 21:15:13 +0200

    V.0.2.0 renamed functions

--- R/foo.R
+++ R/foo.R
@@ -8,11 +8,11 @@
 #' @export
 #'
 #' @examples
-#' mindr()
-mindr <- function(title = 'my title',
+#' md2mm()
+md2mm <- function(title = 'my title',
                   folder = 'mm',
                   remove_curly_bracket = FALSE) {
-  header <- outline(folder, remove_curly_bracket, inputformat)
+  header <- outline(folder, remove_curly_bracket, savefile = FALSE)
   ncc <- sapply(header, function(x) nchar(strsplit(x, split = ' ')[[1]][1]))
   mmtext <- substr(header, ncc + 2, nchar(header))
   mm <- '<map version="1.0.1">'
@@ -28,37 +28,20 @@ mindr <- function(title = 'my title',
   writeLines(text = mm, 'mm.mm', useBytes = TRUE)
 }
 
-#' Extract headers of markdown or rmarkdown files as an outline, or convert a mind map (.mm) into a markdown outline.
+
+#' Convert a mind map (.mm) into a markdown file.
 #'
-#' @param remove_curly_bracket logical.
 #' @param folder character. The folder which contains the input file(s).
-#' @param inputformat character. Its value can be:
-#' - 'markdown' (default)
-#' - 'mindmap'
 #' @param savefile logical
 #' @param savefilename character. Valid when savefile == TRUE
-#'
 #' @return outline of a markdown document or book.
 #' @export
 #'
 #' @examples
-#' outline()
-outline <- function(folder = 'mm',
-                    remove_curly_bracket = FALSE,
-                    inputformat = 'markdown',
-                    savefile = FALSE,
-                    savefilename = 'outline.md') {
-  if (inputformat == 'markdown') {
-    md <- NULL
-    for (filename in dir(folder, full.names = TRUE)) md <- c(md, readLines(filename, encoding = 'UTF-8'))
-    headerloc <- grep('^#+', md)
-    codeloc <- grep('^```', md)
-    # exclude the lines begining with # but in code
-    header <- md[headerloc[!sapply(headerloc, function(x) sum(x > codeloc[seq(1, length(codeloc), by = 2)] & x < codeloc[seq(2, length(codeloc), by = 2)])) == 1]]
-    if (remove_curly_bracket) header <- gsub(pattern = '\\{.*\\}', '', header)
-    if (savefile) writeLines(text = header, savefilename, useBytes = TRUE)
-    return(header)
-  } else if (inputformat == 'mindmap') {
+#' mm2md()
+mm2md <- function(folder = 'mm',
+                  savefile = TRUE,
+                  savefilename = 'mm.md') {
     mm <- NULL
     for (filename in dir(folder, full.names = TRUE)) mm <- c(mm, readLines(filename, encoding = 'UTF-8'))
     mm <- paste0(mm, collapse = '')
@@ -73,7 +56,31 @@ outline <- function(folder = 'mm',
     md[1] <- paste('Title:', md[1])
     if (savefile) writeLines(text = md, savefilename, useBytes = TRUE)
     return(md)
-  } else {
-    message('"inputformat" must be either "markdown" or "mindmap"!')
-  }
+}
+
+#' Extract headers of markdown or rmarkdown files as an outline.
+#'
+#' @param folder character. The folder which contains the input file(s).
+#' @param remove_curly_bracket logical.
+#' @param savefile logical
+#' @param savefilename character. Valid when savefile == TRUE
+#'
+#' @return outline of a markdown document or book.
+#' @export
+#'
+#' @examples
+#' outline()
+outline <- function(folder = 'mm',
+                    remove_curly_bracket = FALSE,
+                    savefile = TRUE,
+                    savefilename = 'outline.md') {
+    md <- NULL
+    for (filename in dir(folder, full.names = TRUE)) md <- c(md, readLines(filename, encoding = 'UTF-8'))
+    headerloc <- grep('^#+', md)
+    codeloc <- grep('^```', md)
+    # exclude the lines begining with # but in code
+    header <- md[headerloc[!sapply(headerloc, function(x) sum(x > codeloc[seq(1, length(codeloc), by = 2)] & x < codeloc[seq(2, length(codeloc), by = 2)])) == 1]]
+    if (remove_curly_bracket) header <- gsub(pattern = '\\{.*\\}', '', header)
+    if (savefile) writeLines(text = header, savefilename, useBytes = TRUE)
+    return(header)
 }
--- readme.md
+++ readme.md
@@ -8,27 +8,38 @@
 devtools::install_github("pzhaonet/mindr")
 ```
 
-### Create a mindmap from (a) markdown file(s).
+### Convert a) markdown file(s) into a mindmap 
 
 Create a folder named `mm` in the working directory (`getwd()`), and drop some markdown or rmarkdown files into `mm/`. Run: 
 
 ```
-mindr::mindr()
+mindr::md2mm()
 ```
 
 then you will get a `mm.mm` file. Open it with any mind-map (brainstorm) software, and you will get a mind map.
 
 Plenty of cross-platform and online mindmap software suits are available (see [the list](https://en.wikipedia.org/wiki/List_of_concept-_and_mind-mapping_software)), among which [freemind](http://freemind.sourceforge.net/wiki/index.php/Download) and [Xmind](http://www.xmind.net/download/win/) are highly recommended.
 
-### Create a markdown file from (a) mind map(s).
+### Convcert (a) mind map(s) into a markdown file 
 
 Drop some mindmap files (.mm) into `mm/`. Run: 
 
 ```
-outline(inputformat = 'mindmap', savefile = TRUE)
+mindr::mm2md()
 ```
 
-then you will get a markdown file.
+then you will get a `mm.md` markdown file.
+
+### Extract the outline from (a) markdown files
+
+Drop some markdown or rmarkdown files into `mm/`. Run: 
+
+```
+mindr::outline()
+```
+
+then you will get an `outline.md` file with headers in it as an outline.
+
 
 Have fun!
 
@@ -50,6 +61,7 @@ Still being developed. Feel free to give your feedback to me!
 
 ## Updates
 
+- 2017-05-21. **Version 0.2.0**. Rename functions.
 - 2017-05-21. **Version 0.1.0**. Bidirectional! Now mind maps can be converted to markdown.
 - 2017-05-20. **Version 0.0.1**. Can Save the outline.
 - 2017-05-19. **Version 0.0.0**. A very preliminary version.
